233764ef9b9a97db8b43ceba660568ce1453fb75
Marian Furnica <mafurnic@c2r7p5.42vienna.com>
2024-09-26 15:53:19 +0200

    edited the main to look like in the subject

--- CPP_01/ex03/main.cpp
+++ CPP_01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mafurnic <mafurnic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 15:39:18 by mafurnic          #+#    #+#             */
-/*   Updated: 2024/09/26 15:48:28 by mafurnic         ###   ########.fr       */
+/*   Updated: 2024/09/26 15:52:42 by mafurnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int main()
 {
     {
     Weapon club = Weapon("crude spiked club");   // Create a weapon
+    
     HumanA bob("Bob", club);   // Create a human with the weapon
     bob.attack();
     club.setType("some other type of club");
@@ -25,11 +26,13 @@ int main()
     }
     {
     Weapon club = Weapon("crude spiked club");
+    
     HumanB jim("Jim");
     jim.setWeapon(club);
     jim.attack();
     club.setType("some other type of club");
     jim.attack();
     }
+    
     return (0);
 }

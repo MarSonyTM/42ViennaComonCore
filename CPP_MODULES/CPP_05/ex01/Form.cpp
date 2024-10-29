b417b5ab2d261016fbc094157df93a55d7af12af
Marian Furnica <mafurnic@c2r7p3.42vienna.com>
2024-10-29 13:02:36 +0100

    working

--- CPP_MODULES/CPP_05/ex01/Form.cpp
+++ CPP_MODULES/CPP_05/ex01/Form.cpp
@@ -6,17 +6,52 @@
 /*   By: mafurnic <mafurnic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 12:42:07 by mafurnic          #+#    #+#             */
-/*   Updated: 2024/10/29 12:50:41 by mafurnic         ###   ########.fr       */
+/*   Updated: 2024/10/29 13:00:41 by mafurnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
+#include "Bureaucrat.hpp"
 
 Form::Form(std::string const &name, int gradeToSign, int gradeToExecute) 
-    : _name(name), _signed(false), _gradeToSign(gradeToSign), _gradeToExecute(gradeToExecute) {
-        
+    : _name(name), _signed(false), _gradeToSign(gradeToSign), _gradeToExecute(gradeToExecute)
+{
         if (gradeToSign < 1 || gradeToExecute < 1)
             throw GradeTooHighException();
         if (gradeToSign > 150 || gradeToExecute > 150)
             throw GradeTooLowException();
-    }
+}
+
+Form::Form(Form const &other)
+    : _name(other._name), _signed(other._signed), _gradeToSign(other._gradeToSign), _gradeToExecute(other._gradeToExecute) {}
+
+Form &Form::operator=(Form const &other)
+{
+    if (this != &other)
+        _signed = other._signed;
+    return (*this);
+}
+
+Form::~Form() {};
+
+std::string const &Form::getName() const 
+{
+    return (_name);
+}
+
+bool Form::isSigned() const
+{
+    return (_signed);
+}
+
+int Form::getGradeToSign() const
+{
+    return (_gradeToSign);
+}
+
+int Form::getGradeToExecute() const
+{
+    return (_gradeToExecute);
+}
+
+ 

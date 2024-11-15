46862dfb92892f78ac91f38b36d83f88a95ec480
Marian Furnica <mafurnic@c2r7p3.42vienna.com>
2024-11-15 12:06:29 +0100

    ex00 done

--- CPP_MODULES/CPP_07/ex00/whatever.hpp
+++ CPP_MODULES/CPP_07/ex00/whatever.hpp
@@ -6,14 +6,13 @@
 /*   By: mafurnic <mafurnic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 11:52:03 by mafurnic          #+#    #+#             */
-/*   Updated: 2024/11/15 11:53:20 by mafurnic         ###   ########.fr       */
+/*   Updated: 2024/11/15 12:00:44 by mafurnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 template <typename T>
-
 void swap(T &a, T &b)
 {
     T tmp = a;
@@ -22,14 +21,12 @@ void swap(T &a, T &b)
 }
 
 template <typename T>
-
 T min(T a, T b)
 {
     return (a < b ? a : b);
 }
 
 template <typename T>
-
 T max(T a, T b)
 {
     return (a > b ? a : b);

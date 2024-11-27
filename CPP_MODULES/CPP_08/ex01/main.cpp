b6c25dc7f25ebee353402ed06ce2996d0dbc8e66
MarSonyTM <56359142+MarSonyTM@users.noreply.github.com>
2024-11-27 14:09:03 +0100

    added more tests to ex01

--- CPP_MODULES/CPP_08/ex01/main.cpp
+++ CPP_MODULES/CPP_08/ex01/main.cpp
@@ -6,74 +6,79 @@
 /*   By: marianfurnica <marianfurnica@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 17:53:07 by marianfurni       #+#    #+#             */
-/*   Updated: 2024/11/27 14:06:50 by marianfurni      ###   ########.fr       */
+/*   Updated: 2024/11/27 14:08:07 by marianfurni      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Span.hpp"
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
-int main() {
+int main()
+{
     try {
-        // Basic test from subject
+        // Test 1: Subject's test case
+        std::cout << "=== Test 1: Subject's test ===" << std::endl;
         Span sp = Span(5);
         sp.addNumber(6);
         sp.addNumber(3);
         sp.addNumber(17);
         sp.addNumber(9);
         sp.addNumber(11);
-
-        std::cout << "Basic test" << std::endl;
+        
         std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
         std::cout << "Longest span: " << sp.longestSpan() << std::endl;
 
-        // Test with 10,000 numbers
+        // Test 2: 10,000 numbers test
+        std::cout << "\n=== Test 2: 10,000 numbers test ===" << std::endl;
         Span bigSpan(10000);
-        for (int i = 0; i < 10000; ++i) {
-            bigSpan.addNumber(i * 2);  // Even numbers from 0 to 19998
+        std::vector<int> numbers;
+        
+        srand(time(NULL));
+        for (int i = 0; i < 10000; i++) {
+            numbers.push_back(rand());
         }
-        std::cout << "\nBig span test:" << std::endl;
+        
+        bigSpan.addRange(numbers.begin(), numbers.end());
         std::cout << "Shortest span: " << bigSpan.shortestSpan() << std::endl;
         std::cout << "Longest span: " << bigSpan.longestSpan() << std::endl;
 
-        // Test addRange
+        // Test 3: Exception cases
+        std::cout << "\n=== Test 3: Exception cases ===" << std::endl;
+        Span smallSpan(1);
+        smallSpan.addNumber(5);
+        try {
+            smallSpan.addNumber(3);  // Should throw exception - span is full
+        }
+        catch (const std::exception& e) {
+            std::cout << "Exception caught: " << e.what() << std::endl;
+        }
+
+        // Test empty span
+        Span emptySpan(5);
+        try {
+            emptySpan.shortestSpan();  // Should throw - no numbers stored
+        }
+        catch (const std::exception& e) {
+            std::cout << "Exception caught: " << e.what() << std::endl;
+        }
+
+        // Test 4: addRange functionality
+        std::cout << "\n=== Test 4: addRange test ===" << std::endl;
         Span rangeSpan(5);
-        std::vector<int> numbers;
-        numbers.push_back(1);
-        numbers.push_back(2);
-        numbers.push_back(3);
-        numbers.push_back(4);
-        numbers.push_back(5);
-        rangeSpan.addRange(numbers.begin(), numbers.end());
-        
-        std::cout << "\nRange test:" << std::endl;
+        std::vector<int> rangeTest;
+        for (int i = 1; i <= 5; ++i)
+            rangeTest.push_back(i);
+            
+        rangeSpan.addRange(rangeTest.begin(), rangeTest.end());
         std::cout << "Shortest span: " << rangeSpan.shortestSpan() << std::endl;
         std::cout << "Longest span: " << rangeSpan.longestSpan() << std::endl;
 
-        // Test exceptions
-        Span fullSpan(1);
-        fullSpan.addNumber(1);
-        fullSpan.addNumber(2);  // Should throw exception
-
-        // Add this test to main.cpp
-        std::vector<int> numbers1;
-        for (int i = 1; i <= 5; ++i)
-            numbers.push_back(i);
-
-        Span sp1(10);
-        try {
-            // Add range of numbers efficiently
-            sp.addRange(numbers.begin(), numbers.end());
-            
-            // Test spans
-            std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
-            std::cout << "Longest span: " << sp.longestSpan() << std::endl;
-        } catch (std::exception& e) {
-            std::cout << "Exception: " << e.what() << std::endl;
-        }
     }
     catch (const std::exception& e) {
-        std::cout << "Exception caught: " << e.what() << std::endl;
+        std::cout << "Unexpected exception: " << e.what() << std::endl;
     }
 
     return 0;

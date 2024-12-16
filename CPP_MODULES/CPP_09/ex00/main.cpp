9de251cc9cb976fb54ae67b0d0d690bdf533a739
MarSonyTM <56359142+MarSonyTM@users.noreply.github.com>
2024-12-16 21:41:39 +0100

    added some comments for clarity

--- CPP_MODULES/CPP_09/ex00/main.cpp
+++ CPP_MODULES/CPP_09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: marianfurnica <marianfurnica@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 15:23:26 by marianfurni       #+#    #+#             */
-/*   Updated: 2024/12/16 17:02:08 by marianfurni      ###   ########.fr       */
+/*   Updated: 2024/12/16 21:39:41 by marianfurni      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        std::cerr << "Error: could not open file.\nUsage: " << argv[0] << " <input_file>" << std::endl;
+        std::cerr << "Error: could not open file.\n Usage: " << argv[0] << " <input_file>" << std::endl;
         return 1;
     }
 
     try {
-        BitcoinExchange exchange("data.csv");
-        exchange.processInputFile(argv[1]);
+        BitcoinExchange exchange("data.csv"); // loading the Bitcoin price database
+        exchange.processInputFile(argv[1]); // processing the input file 
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
         return 1;
@@ -34,8 +34,8 @@ int main(int argc, char *argv[])
 
 /*
      Using std::map because:
-     . perfect for storing data->price mapping
+     . perfect for storing data->price mapping    Date | value
      . automatically sorted by key
      . efficient for searching and retrieval
-     . easy to find the closest date
+     . easy to find the closest date (findClosestDate)
 */
